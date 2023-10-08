776b6a73034e5f0538da9f2eb94f7acead5bed8a
Sachin Suryakanth <sachin.suryakanth@goto.com>
2023-10-08 11:55:05 +0530

    feat: setup redux toolkit and redux store

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { store } from './store.ts'
+import { Provider } from 'react-redux'
 import { App } from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {
@@ -21,6 +23,8 @@ const router = createBrowserRouter(
 )
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
+      <Provider store={store}>
       <RouterProvider router={router} />
+      </Provider>
     </React.StrictMode>
 )
